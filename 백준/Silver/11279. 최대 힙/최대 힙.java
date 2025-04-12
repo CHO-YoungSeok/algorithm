bb8ca6b6d98e5d67a916ddec67db086c80c73827
CYS <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-04-12 23:35:24 +0900

    [Silver II] Title: 최대 힙, Time: 856 ms, Memory: 122468 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,58 +1,28 @@
 import java.util.*;
-
-public class Main{
-
-     static int[] heap = new int[100001];
-     static int size = 0;
-     static StringBuilder sb = new StringBuilder();
-
-     static void re() {
-
-          switch(size) {
-          case 0:
-               sb.append(0 + "\n");
-               break;
-          default:
-               sb.append(heap[1] + "\n");
-               int a = heap[size--];
-               int index = 1;
-               while((index * 2 <= size && a < heap[index * 2]) || (index * 2 + 1 <= size && a < heap[index * 2 + 1])) {
-                    if(index * 2 + 1 <= size && heap[index * 2] < heap[index * 2 + 1]) {
-                         heap[index] = heap[index * 2 + 1];
-                         index = index * 2 + 1;
-                    } else {
-                         heap[index] = heap[index * 2];
-                         index *= 2;
-                    }
-               }
-               heap[index] = a;
-          }
-
-     }
-     static void add(int a) {
-          int index = ++size;
-          while(1 < index && heap[index/2] < a) {
-               heap[index] = heap[index/2];
-               index /= 2;
-          }
-          heap[index] = a;
-     }
+ 
+public class Main {
 
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
-          int t = sc.nextInt();
+          StringBuilder sb = new StringBuilder();
 
+          PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+          int t = sc.nextInt();
           while(0 < t--) {
                int dir = sc.nextInt();
                switch(dir) {
                case 0:
-                    re();
-                    break;
-               default :
-                    add(dir);
+                    switch(heap.size()) {
+                    case 0:
+                         sb.append(0 + "\n");
+                         break;
+                    default:
+                         sb.append(heap.poll() + "\n");
+                    }
+               default:
+                    heap.add(dir);
                }
           }
-
           System.out.println(sb);
      }
 }
