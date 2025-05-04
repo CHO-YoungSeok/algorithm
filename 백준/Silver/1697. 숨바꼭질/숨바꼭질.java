c23ab1f279576088d2a87b508bb0e754a1e38aaa
CYS <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-05-05 02:39:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 18556 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -10,30 +10,31 @@ class Main {
         int n = Integer.parseInt(strs[0]);
         int m = Integer.parseInt(strs[1]);
         boolean[]field = new boolean[100001];
-        int[]distance = new int[100001];
-        distance[n] = 0;
         field[n] = true;
         Queue<Integer> q = new LinkedList<>();
         q.offer(n);
-        
+        int count = 0;
         while (!field[m]) {
-            int currNode = q.remove();
-            if (0 <= currNode -1 && !field[currNode -1]) {
-                field[currNode -1] = true;
-                q.add(currNode -1);
-                distance[currNode -1] = distance[currNode] + 1;
+            int size = q.size();
+            while(0 < size--) {
+                int currNode = q.remove();
+                if (0 <= currNode -1 && !field[currNode -1]) {
+                    field[currNode -1] = true;
+                    q.add(currNode -1);
+                }
+                if (currNode + 1 < 100001 && !field[currNode + 1] ) {
+                    field[currNode +1] = true;
+                    q.add(currNode + 1);
+                }
+                if (currNode * 2 < 100001 && !field[currNode*2]) {
+                    field[currNode * 2] = true;
+                    q.add(currNode * 2);
+                }
             }
-            if (currNode + 1 < 100001 && !field[currNode + 1] ) {
-                field[currNode +1] = true;
-                q.add(currNode + 1);
-                distance[currNode +1] = distance[currNode] + 1;
-            }
-            if (currNode * 2 < 100001 && !field[currNode*2]) {
-                field[currNode * 2] = true;
-                q.add(currNode * 2);
-                distance[currNode * 2] = distance[currNode] + 1;
-            }
+            count++;
         }
-        System.out.println(distance[m]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(count);
+        System.out.println(sb);
     }
 }
