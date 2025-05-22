9f601e99cfd7a5084cf55b36c10ca5e7839beb74
CYS <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-05-22 17:45:47 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -2,39 +2,34 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String[] ins = br.readLine().split(" ");
         int n = Integer.parseInt(ins[0]);
         int r = Integer.parseInt(ins[1]);
         int c = Integer.parseInt(ins[2]);
-
-        long ans = 0;
         int size = 1 << n;
+        long ans = 0;
 
-        while (size > 0) {
+        while (0 < n--) {
             size /= 2;
             if (r < size && c < size) {
-                // 1사분면
-            } else if (r < size && c >= size) {
-                // 2사분면
-                ans += (long) size * size;
-                c -= size;
-            } else if (r >= size && c < size) {
-                // 3사분면
-                ans += 2L * size * size;
-                r -= size;
-            } else {
-                // 4사분면
-                ans += 3L * size * size;
-                r -= size;
-                c -= size;
-            }
 
-            if (size == 0) break;
+            } else if (r < size && c >= size) {
+                c -= size;
+                ans += (size * size) * 1L;
+            } else if (r >= size && c < size) {
+                r -= size;
+                ans += (size * size) * 2L; 
+            } else if (r >= size && c >= size) {
+                c -= size;
+                r -= size;
+                ans += (size * size) * 3L;
+            } else {}
         }
 
         System.out.println(ans);
+
     }
 }
