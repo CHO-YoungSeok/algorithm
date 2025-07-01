18c92de0f12c0dc4c72a44c789d724cd7ef1a6f0
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-07-01 10:28:58 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 13.35 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -4,36 +4,24 @@ class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
         int n = friends.length;
-        int[][] table = new int[n][n];
-        for (int i = 0; i < gifts.length; i++) {
-            String[] pair = gifts[i].split(" ");
-            int taker = -1, giver = -1;
-            for (int j = 0; j < n; j++) {
-                if (pair[0].equals(friends[j])) {
-                    giver = j;
-                    break;
-                }
-            }
-            for (int j = 0; j < n; j++) {
-                if (pair[1].equals(friends[j])) {
-                    taker = j;
-                    break;
-                }
-            }
-            if (giver == -1 || taker == -1) {
-                System.out.println("err");
-            }
-            table[giver][taker]++;       
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(friends[i], i);
         }
         
         int[] degree = new int[n];
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                degree[i] += table[i][j] - table[j][i];
-                degree[j] += table[j][i] - table[i][j];
-            }
-        }
+        int[][] table = new int[n][n];
         
+        for (int i = 0; i < gifts.length; i++) {
+            String[] pair = gifts[i].split(" ");
+            int giver = map.get(pair[0]);
+            int taker = map.get(pair[1]);
+            table[giver][taker]++;
+            degree[giver]++;
+            degree[taker]--;
+        }
+
         int[] thisMonth = new int[n];        
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
