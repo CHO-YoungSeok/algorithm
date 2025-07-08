419cc9b67558a6b618a53284c705c81b74465efe
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-07-08 11:21:50 +0900

    [level 1] Title: 소수 만들기, Time: 154.73 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,37 +1,26 @@
 class Solution {
-    boolean isPrime(int num){
-        if(num == 2 || num == 3) return true;
-        if(num % 2 == 0) return false;
-        for(int i = 3; i <= Math.sqrt(num); i++){
-            if(num%i == 0) return false;
-        }
-        return true;
-    }
-    
     public int solution(int[] nums) {
-        int answer = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-        int len = nums.length;
-        int sum = 0;
-        for(int i = 0; i < len-2; i++){
-            for(int j = i+1; j < len-1; j++){
-                for(int k = j +1; k < len; k++){
-                    sum = nums[i] + nums[j] + nums[k];
-                    if(isPrime(sum)) {
-                        answer++;
-                        System.out.println(nums[i]);
-                        System.out.println(nums[j]);
-                        System.out.println(nums[k]);
-                        System.out.println(sum);
+        int answer = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    boolean bPrime = true;
+                    for (int n = 2; n < sum / 2 + 1; n++) {
+                        if (sum % n == 0) {
+                            bPrime = false;
+                        }
+                    }
+                    if (bPrime) {
+                        System.out.println(count + ": " + i + " " + j + " " + k + " = " + sum);
+                        count++;
                     }
                 }
             }
         }
-        
-        
-
+        answer = count;
+    
         return answer;
     }
 }
