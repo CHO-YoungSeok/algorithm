8fd0bccc9ab4c25d9b511e45910800558091327d
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-07-10 15:26:29 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT
+    distinct o1.user_id, o1.product_id
+from
+    online_sale o1
+    inner join online_sale o2
+        on o1.user_id = o2.user_id
+        and o1.product_id = o2.product_id
+        and o1.online_sale_id != o2.online_sale_id
+order by
+    o1.user_id asc,
+    o1.product_id desc;
+    
