ce8132f42c3b10ccb14c034b6cb4be3b573c27c4
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-07-10 17:31:47 +0900

    Update 쿼드압축 후 개수 세기.java

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,51 +1,36 @@
 class Solution {
-    
-    static int size;
+    int count0, count1;
     
     void division(int[][] arr, int r, int c, int n, int m) {
-            
-        for (int i = r; i < n; i++) {
+        boolean isSame = true;
+        for (int i = r; i < n && isSame; i++) {
             for (int j = c; j < m; j++) {
                 if (arr[r][c] != arr[i][j]) {
-                    int half = (n - r) / 2;
-                    System.out.println(half);
-                    division(arr, r, c, r + half, c + half);
-                    division(arr, r, c + half, r + half, m);
-                    division(arr, r + half, c, n, c + half);
-                    division(arr, r + half, c + half, n, m);
-                    return;
+                    isSame = false;
+                    break;
                 }
             }
         }
         
-        int temp = arr[r][c];
-        for (int i = r; i < n; i++) {
-            for (int j = c; j < m; j++) {
-                arr[i][j] = -1;
-            }
+        if (isSame) {
+            if (arr[r][c] == 0) count0++;
+            else count1++;
+        } else {
+            int half = (n - r) / 2;
+            int halfR = r + half;
+            int halfC = c + half;
+            division(arr, r, c, halfR, halfC);
+            division(arr, r, halfC, halfR, m);
+            division(arr, halfR, c, n, halfC);
+            division(arr, halfR, halfC, n, m);
         }
-        
-        arr[r][c] = temp;
     }
     
     public int[] solution(int[][] arr) {
-        size = arr.length;
-        division(arr, 0, 0, size, size);
-        int count0 = 0, count1 = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                switch (arr[i][j]) {
-                    case 0:
-                        count0++;
-                        break;
-                    case 1:
-                        count1++;
-                        break;
-                    default:
-                }
-            }
-        }
+        count0 = 0; 
+        count1 = 0;
         
+        division(arr, 0, 0, arr.length, arr.length);
         int[] answer = {count0, count1};
         return answer;
     }
