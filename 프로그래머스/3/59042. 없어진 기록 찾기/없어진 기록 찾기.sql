037471cb4f78650b96fa379d7b5c1d2603a311ae
석 <150162806+CHO-YoungSeok@users.noreply.github.com>
2025-08-04 13:31:11 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+SELECT
+    o.animal_id,
+    o.name
+from 
+    animal_outs o
+where
+    not exists (
+        select 1
+        from animal_ins i
+        where i.animal_id = o.animal_id
+    )
+order by
+    o.animal_id,
+    o.name
